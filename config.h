439ffd8a9c912cd58b22674e17bacc48bbbbe511
t16ing <t16ing@users.noreply.github.com>
2020-11-17 00:26:52 +0800

    Change background to black.

--- config.h
+++ config.h
@@ -128,7 +128,7 @@ static const char *colorname[] = {
   [15] = "#ffffff", /* white   */
 
   /* special colors */
-  [256] = "#282a36", /* background */
+  [256] = "#000000", /* background */
   [257] = "#f8f8f2", /* foreground */
 };
 
